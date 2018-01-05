d2542311df5837e1f7319f0530ab5ab30b3b3f84
Geoff McLane <ubuntu@geoffair.info>
2018-01-05 15:34:49 +0100

    Is #3 - Some additions to the 'build-me.bat'

--- build-me.bat
+++ build-me.bat
@@ -1,9 +1,13 @@
 @setlocal
 @REM ####################
 @REM setup
+@set MODMKFIL=1
+@set NEWMKFIL=Makefile.mak
 @set TMPLOG=bldlog-1.txt
 @set BLDDIR=%CD%
 @set TMPROOT=..\..\..
+@set MKFIL=Makefile
+@set TMPTARG=blib\arch\auto\HTML\Tidy\Tidy.dll
 
 @set VCVERS=14
 @set GENERATOR=Visual Studio %VCVERS% Win64
@@ -35,18 +39,39 @@
 @if NOT EXIST Makefile goto NOMAKE
 
 @REM ****************************************************************************************
+@if NOT "%MODMKFIL%x" == "1x" goto DNMKFIL
 @REM Until 'ExtUtils::MakeMaker' can be convinced to **NOT** add `-nodefaultlib` to 'Makefile'
-@echo Doing 'call modmakefile Makefile'
-@echo Doing 'call modmakefile Makefile' >> %TMPLOG%
-@call modmakefile Makefile >> %TMPLOG%
+@echo Doing 'call modmakefile Makefile -o %NEWMKFIL%'
+@echo Doing 'call modmakefile Makefile -o %NEWMKFIL%' >> %TMPLOG%
+@call modmakefile Makefile -o %NEWMKFIL% >> %TMPLOG%
+@set MKFIL=%NEWMKFIL%
+:DNMKFIL
 @REM ****************************************************************************************
-
-@echo Doing 'nmake'
-@echo Doing 'nmake' >> %TMPLOG%
-@nmake >> %TMPLOG% 2>&1
+@if EXIST %TMPTARG% @del %TMPTARG%
+@echo Doing 'nmake /f %MKFIL%'
+@echo Doing 'nmake /f %MKFIL%' >> %TMPLOG%
+@nmake /f %MKFIL% >> %TMPLOG% 2>&1
 @if ERRORLEVEL 1 goto ERR2
-
+@if NOT EXIST %TMPTARG% goto NOBLD
+@echo.
+@call dirmin %TMPTARG%
+@echo Appears a successful build of 'Tidy.dll'
+@echo.
+@echo Do you want to run the 'tests'?
+@echo.
+@pause
+@echo.
+@echo Doing 'nmake /f %MKFIL% test'
+@echo Doing 'nmake /f %MKFIL% test' >> %TMPLOG%
+@nmake /f %MKFIL% test >> %TMPLOG% 2>&1
+@if ERRORLEVEL 1 goto ERR4
+@echo Appears a successful 'test'
+@echo.
 @goto END
+
+:NOBLD
+@echo Error: Appears build of %TMPTARG% FAILED!
+@goto ISERR
 
 :NOBAT
 @echo Error: Can NOT locate %SET_BAT%! *** FIX ME ***
@@ -68,6 +93,10 @@
 
 :ERR2
 @echo Error doing 'nmake'! See %TMPLOG%
+@goto ISERR
+
+:ERR4
+@echo Error doing 'nmake test'! See %TMPLOG%
 @goto ISERR
 
 :ISERR
